5e8bae5b8e0b8f8b16675948d783c9d8be610151
Grace Hseu <25470155+gracehseu@users.noreply.github.com>
2020-12-04 01:49:51 -0600

    fixing routing

--- app/module_ex_1/controllers.py
+++ app/module_ex_1/controllers.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template
 
 module_ex_1_bp = Blueprint('module_ex_1', __name__)
 
-@module_ex_1_bp("/module_ex_1_bp", methods=["GET", "POST"])
-@module_ex_1_bp("/", methods=["GET", "POST"])
+@module_ex_1_bp.route("/module_ex_1_bp", methods=["GET", "POST"])
+@module_ex_1_bp.route("/", methods=["GET", "POST"])
 def main():
     print('in module_ex_1_bp')
     return render_template("module_ex_1/module.html")
