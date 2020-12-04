98a94015665f543428518c0d3001ece1f281e3e4
Grace Hseu <25470155+gracehseu@users.noreply.github.com>
2020-12-04 01:46:16 -0600

    fixed importing from module

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
 # importing blueprints from modules
-from .module_ex_1 import module_ex_1
-from .module_ex_2 import module_ex_2
+from .module_ex_1.controllers import module_ex_1_bp
+from .module_ex_2.controllers import module_ex_2_bp
 
 # creating app
 app = Flask(__name__)
